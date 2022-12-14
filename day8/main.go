b48faca75d8b3e3004b0a7c67dd452b3ff5867f4
James Allen <24575899+jrdallen97@users.noreply.github.com>
2022-12-14 08:42:26 +0000

    Day 8 (part 2)

--- day8/main.go
+++ day8/main.go
@@ -46,4 +46,56 @@ func main() {
 	v.Print(Right)
 
 	fmt.Println("visible trees:", v.CountVisible())
+	scores := map[string]int{}
+	for y := range lines {
+		for x := range lines[y] {
+			coord := fmt.Sprintf("%d,%d", x, y)
+			scores[coord] = calcScore(lines, x, y)
+		}
+	}
+
+	max := 0
+	for coord, val := range scores {
+		if val > max {
+			max = val
+			fmt.Println("new max:", coord, val)
+		}
+	}
+	fmt.Println("max:", max)
+}
+
+// not my best work lol
+func calcScore(lines [][]int, x, y int) int {
+	self := lines[y][x]
+	up, left, right, down := 0, 0, 0, 0
+
+	for i := x + 1; i < len(lines[y]); i++ {
+		right++
+		if lines[y][i] >= self {
+			break
+		}
+	}
+
+	for i := y + 1; i < len(lines); i++ {
+		down++
+		if lines[i][x] >= self {
+			break
+		}
+	}
+
+	for i := x - 1; i >= 0; i-- {
+		left++
+		if lines[y][i] >= self {
+			break
+		}
+	}
+
+	for i := y - 1; i >= 0; i-- {
+		up++
+		if lines[i][x] >= self {
+			break
+		}
+	}
+
+	return up * left * right * down
 }
